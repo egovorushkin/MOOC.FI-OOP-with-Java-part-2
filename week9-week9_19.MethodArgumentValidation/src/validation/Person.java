70329b1dad741678c1f44d51979a531eacdb91e3
Evgenii Govorushkin <johnnywas87@mail.com>
2019-02-03 14:43:34 +0300

    solution

---
+++ week9-week9_19.MethodArgumentValidation/src/validation/Person.java
@@ -0,0 +1,27 @@
+package validation;
+
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        if(name == null || name.isEmpty() || name.length() > 40){
+            throw new IllegalArgumentException("Name or age is invalid");
+        }
+
+        if(age < 0 || age > 120){
+            throw new IllegalArgumentException("The age has to be between 0-120");
+        }
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
